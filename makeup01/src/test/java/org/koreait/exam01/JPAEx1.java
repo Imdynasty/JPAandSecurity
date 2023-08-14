322da3569c26b24f491587104d386a51df8082a9
Imdynasty <osm05230@gmail.com>
2023-08-14 22:52:27 +0900

    JPA 0811 2번째 강의 10분경

---
+++ makeup01/src/test/java/org/koreait/exam01/JPAEx1.java
@@ -0,0 +1,35 @@
+package org.koreait.exam01;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.koreait.entities.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations="classpath:application-test.properties")
+public class JPAEx1 {
+
+    @Autowired
+    private EntityManager em;
+
+
+    @Test
+    void test3() {
+        Users users = new Users();
+        users.setUserNo(2L);
+        users.setUserId("user01");
+        users.setUserPw("12345678");
+        users.setUserNm("사용자01");
+        users.setEmail("user01@test.org");
+        users.setMobile("0100000");
+
+        em.persist(users); // 영속성에 추가 - 변화 감지
+        em.flush();
+
+
+    }
+}
